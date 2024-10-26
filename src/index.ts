389e5baaa4a7c77cd4e826c4233483e2bfee76be
Abass Hammed <139161544+AbassHammed@users.noreply.github.com>
2024-10-26 17:02:26 +0200

    Added some docker stuff will need to optimize in the future

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 const app = express();
-const port = 3000;
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  return console.log(`Express is listening at http://localhost:${PORT}`);
 });
