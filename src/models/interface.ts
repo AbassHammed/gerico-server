a52d6f7a23f31cbde2560baf25b45e220f6b6703
Abass Hammed <139161544+AbassHammed@users.noreply.github.com>
2024-10-27 16:26:49 +0100

    Created the UserRepository

--- src/models/interface.ts
+++ src/models/interface.ts
@@ -39,3 +39,53 @@ export interface ITest extends RowDataPacket {
   age: number;
   city: string;
 }
+
+/**
+ * Generic repository interface to provide basic CRUD operations
+ * @example
+ * ```typescript
+ * interface ITestRepository<T> extends Required<IRepository<T>>
+ * ```
+ * this makes the `IRepository` methods compulsory to the extending interface
+ */
+export interface IRepository<T> {
+  /**
+   * Saves a new entity to the repository.
+   * @param {T} t - The entity to save
+   * @returns {Promise<T>} - A promise that resolves to `true` indicating the save was successfull else an error is thrown
+   */
+  save?(t: T): Promise<true>;
+
+  /**
+   * Retrieves all entities from the repository.
+   * @returns {Promise<T[]>} - A promise that resolves to an array of entities
+   */
+  retrieveAll?(): Promise<T[]>;
+
+  /**
+   * Retrieves a single entity by its ID.
+   * @param {string | number} id - The ID of the entity to retrieve
+   * @returns {Promise<T | undefined>} - A promise that resolves to the entity or undefined if not found
+   */
+  retrieveById?(id: string | number): Promise<T | undefined>;
+
+  /**
+   * Updates an existing entity in the repository.
+   * @param {T} t - The entity to update
+   * @returns {Promise<true>} - A promise that resolves to `true` indicating the update was successfull else an error is thrown
+   */
+  update?(t: T): Promise<true>;
+
+  /**
+   * Deletes an entity by its ID.
+   * @param {string | number} t - The ID of the entity to delete
+   * @returns {Promise<number>} - A promise that resolves to `true` indicating the delete was successfull else an error is thrown
+   */
+  delete?(t: string | number): Promise<true>;
+
+  /**
+   * Deletes all entities in the repository.
+   * @returns {Promise<number>} - A promise that resolves to the count of deleted records
+   */
+  deleteAll?(): Promise<number>;
+}
---
+++ src/repositories/user.ts
@@ -0,0 +1,133 @@
+import { IRepository, IUser } from '../models/interface';
+import connection from '../models/connect';
+import { ResultSetHeader } from 'mysql2';
+
+class UserRepository implements Required<IRepository<IUser>> {
+  save(t: IUser): Promise<true> {
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>(
+        'INSERT INTO users (uid, first_name, last_name, phone_number, email, password, hire_date, created_at, user_post, updated_at, departure_date, is_admin, is_archived, reset_code) VALUES(?, ?,?,?,?,?,?,?,?,?,?,?,?,?)',
+        [
+          t.uid,
+          t.first_name,
+          t.last_name,
+          t.phone_number,
+          t.email,
+          t.password,
+          t.hire_date,
+          t.created_at,
+          t.user_post,
+          t.updated_at,
+          t.departure_date,
+          t.is_admin,
+          t.is_archived,
+          t.reset_code,
+        ],
+        err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        },
+      );
+    });
+  }
+
+  retrieveAll(): Promise<IUser[]> {
+    return new Promise((resolve, reject) => {
+      connection.query<IUser[]>('SELECT * FROM users', (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  }
+
+  retrieveById(id: string | number): Promise<IUser> {
+    return new Promise((resolve, reject) => {
+      connection.query<IUser[]>('SELECT * FROM users WHERE uid = ?', [id], (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res?.[0]);
+        }
+      });
+    });
+  }
+
+  update(t: IUser): Promise<true> {
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>(
+        'UPDATE users SET first_name = ?, last_name = ?, phone_number = ?, email = ?, password = ?, hire_date = ?, created_at = ?, user_post = ?, updated_at = ?, departure_date = ?, is_admin = ?, is_archived = ?, reset_code = ? WHERE uid = ?',
+        [
+          t.first_name,
+          t.last_name,
+          t.phone_number,
+          t.email,
+          t.password,
+          t.hire_date,
+          t.created_at,
+          t.user_post,
+          t.updated_at,
+          t.departure_date,
+          t.is_admin,
+          t.is_archived,
+          t.reset_code,
+          t.uid,
+        ],
+        err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        },
+      );
+    });
+  }
+
+  delete(id: string | number): Promise<true> {
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>('DELETE FROM users WHERE uid = ?', [id], err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  archive(id: string | number): Promise<true> {
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>(
+        'UPDATE users SET is_archived = true WHERE uid = ?',
+        [id],
+        err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        },
+      );
+    });
+  }
+
+  deleteAll(): Promise<number> {
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>('DELETE FROM users', err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(1);
+        }
+      });
+    });
+  }
+}
+
+export default new UserRepository();
